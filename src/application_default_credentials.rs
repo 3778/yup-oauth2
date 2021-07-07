f5e19ec685d7be77b3dde2084f21f859701cbd81
Lucas Campiolo Paiva <lucas.paiva@3778.care>
2021-07-07 12:03:28 -0300

    Fix hyper request to metadata server

--- src/application_default_credentials.rs
+++ src/application_default_credentials.rs
@@ -23,7 +23,7 @@ impl ApplicationDefaultCredentialsFlow {
         let token_uri = format!("http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token?scopes={}", scope);
         let request = hyper::Request::get(token_uri)
             .header("Metadata-Flavor", "Google")
-            .body(hyper::Body::from(String::new())) // why body is needed?
+            .body(hyper::Body::empty())
             .unwrap();
         log::debug!("requesting token from metadata server: {:?}", request);
         let (head, body) = hyper_client.request(request).await?.into_parts();
